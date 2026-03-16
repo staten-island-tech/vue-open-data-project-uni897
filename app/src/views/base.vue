2891fb3158a593f403dd40b7427c9a5bcdf9b0b4
uni897 <akhasmeer7@gmail.com>
2026-03-16 14:02:34 -0400

    gdaasdgasdg

---
+++ app/src/views/base.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const rests = ref({})
+async function getData() {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/43nn-pn8j.json')
+    const data = await response.json()
+    rests.value = data.results
+  } catch (error) {
+    console.log(error)
+  }
+}
+onMounted(() => {
+  getData()
+})
+</script>
+
+<style scoped>
+
+</style>
