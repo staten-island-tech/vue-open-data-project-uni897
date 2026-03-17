53f65b16cd91e44ab170ba558e9b700089bed490
uni897 <akhasmeer7@gmail.com>
2026-03-17 18:58:21 -0400

    sumohwakemepapa

---
+++ app/src/components/RestuarantCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-150 h-50 font-bold bg-teal-300 rounded-2xl border-10 border-teal-500 mt-5 p-1">
+        <h1>Name: {{ props.rest.dba }}</h1>
+        <h1>Address: {{ props.rest.building }} {{ props.rest.street }}</h1>
+        <h1>Criticality: {{ props.rest.critical_flag }}</h1>
+        <h1 v-if="props.rest.critical_flag === 'Critical'">Description: {{ props.rest.violation_description }}</h1>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    rest:{
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
--- app/src/views/base.vue
+++ app/src/views/base.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-      <select v-if="loaded">
+    <div class="m-5">      
+      <h1 v-if="!checkSelect">Please select a restuarant</h1>
+      <select @change="getCritical" v-if="loaded" v-model="selected" class="bg-teal-300 border-3 border-teal-500">
         <option>--Select--</option>
-        <Options v-for="rest in rests" :key="rest.dba" :rest="rest"></Options>
+        <option v-for="rest in rests" :key="rest.dba" :value="rest">{{ rest.dba }}</option>
       </select>
+      <RestuarantCard v-if="checkSelect" :rest="selected"/>
     </div>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue'
-import Options from '@/components/options.vue'
-const rests = ref({})
+import { ref, onBeforeMount, computed } from 'vue'
+import RestuarantCard from '@/components/RestuarantCard.vue'
+const rests = ref()
 const loaded = ref(false)
+const selected = ref("--Select--")
+
+const checkSelect = computed(() => selected.value !== "--Select--")
+
+function getCritical(){
+  console.log(selected.value.critical_flag)
+}
 async function getData() {
   try {
     const response = await fetch('https://data.cityofnewyork.us/resource/43nn-pn8j.json')
