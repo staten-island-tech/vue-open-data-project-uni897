d8ab9e339201dd2dd2d03eb7c75079be2f3f83a1
uni897 <akhasmeer7@gmail.com>
2026-03-17 14:01:57 -0400

    saveyourworkcommittogithub

---
+++ app/src/components/options.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <option>{{ props.rest.dba }}</option>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    rest:{
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
--- app/src/views/base.vue
+++ app/src/views/base.vue
@@ -1,24 +1,32 @@
 <template>
     <div>
-
+      <select v-if="loaded">
+        <option>--Select--</option>
+        <Options v-for="rest in rests" :key="rest.dba" :rest="rest"></Options>
+      </select>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onBeforeMount } from 'vue'
+import Options from '@/components/options.vue'
 const rests = ref({})
+const loaded = ref(false)
 async function getData() {
   try {
     const response = await fetch('https://data.cityofnewyork.us/resource/43nn-pn8j.json')
     const data = await response.json()
-    rests.value = data.results
+    rests.value = data
+    loaded.value = true
   } catch (error) {
     console.log(error)
   }
 }
-onMounted(() => {
+onBeforeMount(() =>
+ {
   getData()
 })
+
 </script>
 
 <style scoped>
